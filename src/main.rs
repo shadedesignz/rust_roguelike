0697f5ae7ffbc991da74f0fd7eb743afec58ee58
Joe Beltramo <proshadedesignz@gmail.com>
2019-10-27 14:39:52 -0700

    Support initial movements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 // Actual window size
 const SCREEN_WIDTH: i32 = 80;
@@ -24,11 +26,45 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
     }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        // Movement
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        },
+        // Exit the game
+        Key { code: Escape, .. } => return true,
+
+        _ => {}
+    }
+
+    false
 }
