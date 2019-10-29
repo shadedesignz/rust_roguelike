3f5732cf21a480a8a52ec236611843d06e4c021e
Joe Beltramo <proshadedesignz@gmail.com>
2019-10-28 22:56:09 -0700

    Add object placement method and update block checking

--- src/object.rs
+++ src/object.rs
@@ -1,5 +1,9 @@
-use tcod::{Color, Console, BackgroundFlag};
-use crate::map::Game;
+use tcod::{Color, Console, BackgroundFlag, colors};
+use crate::map::{Rect, Map};
+use rand::Rng;
+
+const MAX_ROOM_MONSTERS: i32 = 3;
+
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum PlayerAction {
     TookTurn,
@@ -31,10 +35,10 @@ impl Object {
         }
     }
 
-    pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
-        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
-            self.x += dx;
-            self.y += dy;
+    pub fn move_by(id: usize, dx: i32, dy: i32, map: &Map, objects: &mut [Object]) {
+        let (x, y) = objects[id].pos();
+        if !is_blocked(x + dx, y + dy, map, objects) {
+            objects[id].set_pos(x + dx, y + dy);
         }
     }
 
@@ -50,5 +54,36 @@ impl Object {
     pub fn set_pos(&mut self, x: i32, y: i32) {
         self.x = x;
         self.y = y;
+    }
+}
+
+pub fn is_blocked(x: i32, y: i32, map: &Map, objects: &[Object]) -> bool {
+    if map[x as usize][y as usize].blocked {
+        return true;
+    }
+
+    objects
+        .iter()
+        .any(|object| object.blocks && object.pos() == (x, y))
+}
+
+pub fn place_objects(room: Rect, map: &Map, objects: &mut Vec<Object>) {
+    let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOM_MONSTERS + 1);
+
+    for _ in 0..num_monsters {
+        let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
+        let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
+
+        // 0.8 = 80% chance of getting an orc
+        if !is_blocked(x, y, map, objects) {
+            let mut monster = if rand::random::<f32>() < 0.8 {
+                Object::new(x, y, 'o', "orc", colors::DESATURATED_GREEN, true)
+            } else {
+                Object::new(x, y, 'T', "troll", colors::DARKER_GREEN, true)
+            };
+
+            monster.alive = true;
+            objects.push(monster);
+        }
     }
 }
